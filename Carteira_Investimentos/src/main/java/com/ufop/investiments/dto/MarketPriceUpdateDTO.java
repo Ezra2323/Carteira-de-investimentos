da325df98539ff189019f3c238c1f287154c07a9
ezra2323 <acaavila@gmail.com>
2026-02-23 10:43:38 -0300

    Removendo pasta

--- Carteira_Investimentos/src/main/java/com/ufop/investiments/dto/MarketPriceUpdateDTO.java
+++
@@ -1,9 +0,0 @@
-package com.ufop.investiments.dto;
-
-import lombok.Data;
-import java.math.BigDecimal;
-
-@Data
-public class MarketPriceUpdateDTO {
-    private BigDecimal currentPrice;
-}
--- Carteira_Investimentos/src/main/java/com/ufop/investiments/enums/AssetType.java
+++
@@ -1,9 +0,0 @@
-package com.ufop.investiments.enums;
-
-public enum AssetType {
-    ACAO,
-    CRIPTO,
-    FUNDO,
-    RENDA_FIXA,
-    OUTRO
-}
